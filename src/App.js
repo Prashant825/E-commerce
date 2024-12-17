7e4f8bd9944b9623bff9d75250e52997c3f210af
Prashant Lambha <125307000+Prashant825@users.noreply.github.com>
2024-12-17 23:34:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import './Ecomm.css';
+import { Button } from '@mui/material';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button  variant="contained" color='info'>Hello React</Button>
+      </header>
+      
+    </div>
+  );
+}
+export default App;
